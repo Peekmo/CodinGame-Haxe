33fe48ebd5455015a6b67d64f808f046c37de87c
Peekmo <axel.anceau@gmail.com>
2014-03-18 22:46:55 +0100

    Roller Coaster updated

--- CodinGame/JS/RollerCoaster.js
+++ CodinGame/JS/RollerCoaster.js
@@ -5,58 +5,41 @@ HxOverrides.cca = function(s,index) {
 	if(x != x) return undefined;
 	return x;
 }
-HxOverrides.iter = function(a) {
-	return { cur : 0, arr : a, hasNext : function() {
-		return this.cur < this.arr.length;
-	}, next : function() {
-		return this.arr[this.cur++];
-	}};
-}
 var RollerCoaster = function() { }
-RollerCoaster.doIt = function(nb) {
-	if(nb + RollerCoaster.current > RollerCoaster.l) {
-		if(RollerCoaster.did == RollerCoaster.c) return false;
-		RollerCoaster.wagons = 0;
-		RollerCoaster.current = 0;
-		RollerCoaster.did = RollerCoaster.did + 1;
-	} else if(RollerCoaster.wagons == RollerCoaster.n) {
-		if(RollerCoaster.did == RollerCoaster.c) return false;
-		RollerCoaster.wagons = 0;
-		RollerCoaster.current = 0;
-		RollerCoaster.did = RollerCoaster.did + 1;
-	}
-	RollerCoaster.wagons = RollerCoaster.wagons + 1;
-	RollerCoaster.total = RollerCoaster.total + nb;
-	RollerCoaster.current = RollerCoaster.current + nb;
-	return true;
-}
 RollerCoaster.main = function() {
 	var line = readline().split(" ");
-	RollerCoaster.l = Std.parseInt(line[0]);
-	RollerCoaster.c = Std.parseInt(line[1]);
-	RollerCoaster.n = Std.parseInt(line[2]);
+	var l = Std.parseInt(line[0]);
+	var c = Std.parseInt(line[1]);
+	var n = Std.parseInt(line[2]);
 	var groups = new Array();
-	var _g1 = 0, _g = RollerCoaster.n;
-	while(_g1 < _g) {
-		var i = _g1++;
-		var group = Std.parseInt(readline());
-		groups.push(group);
-		if(!RollerCoaster.doIt(group)) {
-			RollerCoaster.done = true;
-			break;
-		}
+	var _g = 0;
+	while(_g < n) {
+		var i = _g++;
+		groups.push(Std.parseInt(readline()));
 	}
-	while(!RollerCoaster.done) {
-		var $it0 = HxOverrides.iter(groups);
-		while( $it0.hasNext() ) {
-			var i = $it0.next();
-			if(!RollerCoaster.doIt(i)) {
-				RollerCoaster.done = true;
-				break;
-			}
+	var tours = new Array();
+	var _g = 0;
+	while(_g < n) {
+		var i = _g++;
+		var limit = l;
+		var pos = i;
+		while(true) {
+			if(limit - groups[pos] < 0) break; else limit = limit - groups[pos];
+			pos++;
+			if(pos == n) pos = 0;
+			if(pos == i) break;
 		}
+		tours.push({ pos : pos, gain : l - limit});
 	}
-	print(RollerCoaster.total);
+	var total = 0;
+	var pos = 0;
+	var _g = 0;
+	while(_g < c) {
+		var i = _g++;
+		total = total + tours[pos].gain;
+		pos = tours[pos].pos;
+	}
+	print(total);
 }
 var Std = function() { }
 Std.parseInt = function(x) {
@@ -65,10 +48,5 @@ Std.parseInt = function(x) {
 	if(isNaN(v)) return null;
 	return v;
 }
-RollerCoaster.current = 0;
-RollerCoaster.total = 0;
-RollerCoaster.did = 1;
-RollerCoaster.wagons = 0;
-RollerCoaster.done = false;
 RollerCoaster.main();
 })();
--- CodinGame/RollerCoaster.hx
+++ CodinGame/RollerCoaster.hx
@@ -1,69 +1,53 @@
+typedef Tour = {
+    var pos: Int;
+    var gain: Int;
+}
+
 class RollerCoaster
 {
-    public static var l : Int;
-    public static var c : Int;
-    public static var n : Int;
-
-    public static var current : Int = 0;
-    public static var total : Int = 0;
-    public static var did : Int = 1;
-
-    public static var wagons : Int = 0;
-
-    public static var done : Bool = false;
-
-    public static function doIt(nb: Int) : Bool
-    {
-        if (nb+current > l) {
-            if (did == c) {
-                return false;
-            }
-
-            wagons = 0;
-            current = 0;
-            did = did + 1;
-        } else if (wagons == n) {
-            if (did == c) {
-                return false;
-            }
-
-            wagons = 0;
-            current = 0;
-            did = did + 1;
-        }
-        
-        wagons = wagons + 1;
-        total = total + nb;
-        current = current + nb;
-
-        return true;
-    }
-
     public static function main()
     {
         var line : Array<String> = '{INPUT}'.split(' ');
-        l = Std.parseInt(line[0]);
-        c = Std.parseInt(line[1]);
-        n = Std.parseInt(line[2]);
+        var l : Int = Std.parseInt(line[0]);
+        var c : Int = Std.parseInt(line[1]);
+        var n : Int = Std.parseInt(line[2]);
 
         var groups : Array<Int> = new Array<Int>();
         for (i in 0...n) {
-            var group : Int = Std.parseInt('{INPUT}');
-            groups.push(group);
-
-            if (!doIt(group)) {
-                done = true;
-                break;
-            } 
+            groups.push(Std.parseInt('{INPUT}'));
         }
 
-        while (!done) {
-            for (i in groups.iterator()) {
-                if (!doIt(i)) {
-                    done = true;
+        var tours : Array<Tour> = new Array<Tour>();
+        for (i in 0...n) {
+            var limit : Int = l;
+            var pos : Int = i;
+
+            while (true) {
+                if (limit - groups[pos] < 0) {
                     break;
-                }        
+                } else {
+                    limit = limit - groups[pos];
+                }
+
+                pos++;
+
+                if (pos == n) {
+                    pos = 0;
+                }
+
+                if (pos == i) {
+                    break;
+                }
             }
+
+            tours.push({pos: pos, gain: l - limit});
+        }
+
+        var total : Int = 0;
+        var pos : Int = 0;
+        for (i in 0...c) {
+            total = total + tours[pos].gain;
+            pos = tours[pos].pos;
         }
 
         trace(total);
